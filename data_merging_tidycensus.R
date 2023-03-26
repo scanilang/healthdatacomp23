2b9566fc0d81effebf35d03d569f506b332f7359
mehtatanvi <tanvi.mehta2@gmail.com>
2023-03-26 17:08:20 -0500

    updated data

--- data_merging_tidycensus.R
+++ data_merging_tidycensus.R
@@ -3,7 +3,7 @@ library(tidycensus)
 library(readxl)
 library(dplyr)
 
-# utilization_data <- read_excel("Data/BACH Competition Data Set_3.2.2023/BACH Competition_Cost & Utilization Data Set_3.2.2023.xlsx")
+utilization_data <- read_excel("Data/BACH Competition Data Set_3.2.2023/BACH Competition_Cost & Utilization Data Set_3.2.2023.xlsx") %>% filter(`Measurement Year` == 2021) %>% select(`Medical Group Name`, `Adults TCOC`)
 quality_data_adj <- read_excel("Data/BACH Competition Data Set_3.2.2023/BACH Competition_Quality Measures Data Set_3.2.2023.xlsx", sheet = 1)
 # quality_data_unadj <- read_excel("Data/BACH Competition Data Set_3.2.2023/BACH Competition_Quality Measures Data Set_3.2.2023.xlsx", sheet = 2)
 
@@ -16,7 +16,18 @@ quality_data_adj <- quality_data_adj %>%
                                    `Clinic City`  == "Saint Louis Park" ~ "St. Louis Park",
                                    `Clinic City`  == "Rpbbinsdale" ~ "Robbinsdale",
                                    TRUE ~ `Clinic City`),
-         `Clinic Name` = ifelse(`Clinic Name` == 'Centracare Clinic - SCMG St. Cloud Medical Group â€“ South', "Centracare Clinic - SCMG St. Cloud Medical Group South", str_trim(`Clinic Name`)))
+         `Clinic Name` = ifelse(`Clinic Name` == 'Centracare Clinic - SCMG St. Cloud Medical Group â€“ South', "Centracare Clinic - SCMG St. Cloud Medical Group South", str_trim(`Clinic Name`)),
+         `Medical Group Name` = case_when(str_detect(`Clinic Name`, "EH East") == TRUE ~ "Essentia Health - East Region", # in 2019 it is East not East Region
+                                               str_detect(`Clinic Name`, "EH West") == TRUE ~ "Essentia Health - West",
+                                               `Clinic Name` == "Mayo Clinic Health System Fairmont" ~ "Mayo Clinic Health System - Fairmont",
+                                              `Clinic Name` == "Mayo Clinic Health System- Franciscan Healthcare in Caledonia" ~ "Mayo Clinic Health System - Franciscan Healthcare in La Cr",
+                                              `Clinic Name` == "Mayo Clinic Health System Lake City" ~ "Mayo Clinic Health System - Lake City",
+                                          `Clinic Name` == "Mayo Clinic - Northwest" ~ "Mayo Clinic Health System - Northwest Wisconsin Region",
+                                          `Clinic Name` == "Mayo Clinic Health System Red Wing" ~ "Mayo Clinic Health System - Red Wing",
+                                          `Clinic Name` == "Mayo Family Clinic Southeast" ~ "Mayo Clinic Health System - Southeast Minnesota Region",
+                                          `Clinic Name` == "Mayo Clinic Health System St. James" ~ "Mayo Clinic Health System - St. James",
+                                          `Clinic Name` == "Mayo Clinic Health System Owatonna" ~ "Mayo Clinic Health System-Owatonna ",
+                                               TRUE ~ `Medical Group Name`))
 
 data_phq9_6month <- quality_data_adj %>% 
   dplyr::filter(`Measure Name` == "PHQ-9 Follow-up Rate at 6 Months Adult" & `Clinic Name` != "TOTAL") %>%
@@ -94,6 +105,50 @@ insurance_coverage <- get_acs(
   mutate(insurance_coverage_rate = covered/estimate.y.y) %>%
   select(GEOID, insurance_coverage_rate)
 
+workplace <- get_acs(
+  geography = "zip code tabulation area", 
+  variables = "C24070_053",
+  year = 2021) %>%
+  left_join(get_acs(
+    geography = "zip code tabulation area", 
+    variables = "C24070_054",
+    year = 2021), by = "GEOID") %>%
+  left_join(get_acs(
+    geography = "zip code tabulation area", 
+    variables = "C24070_056",
+    year = 2021), by = "GEOID") %>%
+  mutate(public_workplace = estimate.x + estimate.y + estimate) %>%
+  left_join(population, by = "GEOID") %>%
+  mutate(workplace_rate = public_workplace/estimate.y.y) %>%
+  select(GEOID, workplace_rate)
+
+public_transit_to_work <- get_acs(
+  geography = "zip code tabulation area", 
+  variables = "B08301_010",
+  year = 2021
+) %>%
+  left_join(population, by = "GEOID") %>%
+  mutate(public_transit_to_work_rate = estimate.x/estimate.y) %>%
+  select(GEOID, public_transit_to_work_rate)
+
+public_transit_to_work <- get_acs(
+  geography = "zip code tabulation area", 
+  variables = "B08301_010",
+  year = 2021
+) %>%
+  left_join(population, by = "GEOID") %>%
+  mutate(public_transit_to_work_rate = estimate.x/estimate.y) %>%
+  select(GEOID, public_transit_to_work_rate)
+
+aggregate_travel_time <- get_acs(
+  geography = "zip code tabulation area", 
+  variables = "B08013_001",
+  year = 2021
+) %>%
+  left_join(population, by = "GEOID") %>%
+  mutate(aggregate_travel_time_rate = estimate.x/estimate.y) %>%
+  select(GEOID, aggregate_travel_time_rate)
+
 
 covid <- read.csv("Data/covid_zip.csv") %>% 
   select(ZIP, Cases) %>%
@@ -142,7 +197,9 @@ all_data <- left_join(data_phq9_6month, census_data, by = c("ZIP" = "GEOID")) %>
             TRUE ~ covid_rate)) %>%
   left_join(classification, by = c("Clinic Name" = "Clinic.Name")) %>%
   left_join(zip_data, by = c(`Clinic Zip Code` = "Zip.Code")) %>%
-  left_join(rural_classification, by = "County")
+  left_join(rural_classification, by = "County") %>%
+  left_join(utilization_data, by = "Medical Group Name")
+
 
 write.csv(all_data, "Data/tidycensus_data.csv")
 
@@ -167,7 +224,8 @@ write.csv(all_data, "Data/tidycensus_data.csv")
 
 mod1 <- lm(data = all_data, `Actual Rate` ~ as.factor(`Measurement Year`) + home_ownership_rate +
              educ_attainment_rate + private_vehicle_rate + insurance_coverage_rate + 
-             covid_rate + public_fqhc_ind + as.factor(code))
+             covid_rate + public_fqhc_ind + as.factor(code) + `Adults TCOC` + workplace_rate + 
+             public_transit_to_work_rate + aggregate_travel_time_rate)
 summary(mod1)
 
 
